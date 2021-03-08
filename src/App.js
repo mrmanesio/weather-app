3e07ebc9c75cb3fd08632284d18438de058351f8
manesio2009@gmail.com <manesio2009@gmail.com>
2021-03-08 23:47:31 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import axios from 'axios';
+import React from 'react';
 
 const App = () => {
+
+  const proxy = 'https://cors-anywhere.herokuapp.com/';
+
+  React.useEffect(() => {
+    axios
+      .get(
+        `${proxy}pro.openweathermap.org/data/2.5/forecast/hourly?q=Dnipro&appid=b8988efec93cd285602a1f2dda449964`,
+      )
+      .then((responce) => console.log(responce));
+  }, []);
+
   return (
     <div className="App">
       kuku

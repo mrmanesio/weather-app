c6adb1c0a55afa5766af26db65205ff153da65f7
manesio2009@gmail.com <manesio2009@gmail.com>
2021-03-10 23:07:26 +0200

    add backgrounds

--- src/App.js
+++ src/App.js
@@ -2,32 +2,34 @@ import React from 'react';
 // import axios from 'axios';
 
 const App = () => {
-
   const API_key = 'b8988efec93cd285602a1f2dda449964';
+  const proxy = 'https://cors-anywhere.herokuapp.com/';
   const [loading, setLoading] = React.useState(true);
-  const url = (location) => (`https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_key}`)
-
-
+  const [data, setData] = React.useState({});
+  const url = (location) =>
+    `${proxy}api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_key}`;
 
   React.useEffect(() => {
     const getWetherByLocation = async (location) => {
-      setLoading(true);
       const resp = await fetch(url(location));
       const respData = await resp.json();
-
-      console.log(respData);
-    }
-
-      getWetherByLocation('dnipro');
+      setData(respData);
       setLoading(false);
+    };
+
+    getWetherByLocation('dnipro');
   }, []);
 
   console.log(loading);
+
   return (
     <div className="App">
-      kuku
+      {data?.name}
+      {Math.round(data?.main?.temp - 273)}
+      feels like
+      {Math.round(data?.main?.feels_like - 273)}
     </div>
   );
-}
+};
 
 export default App;

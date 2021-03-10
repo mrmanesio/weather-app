deb93f916f8f044a4c640f53fe744326db871b01
Alexey <manesio2009@gmail.com>
2021-03-10 17:50:03 +0200

    add fetch

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import axios from 'axios';
 import React from 'react';
+// import axios from 'axios';
 
 const App = () => {
 
-  const proxy = 'https://cors-anywhere.herokuapp.com/';
+  const API_key = 'b8988efec93cd285602a1f2dda449964';
+  const [loading, setLoading] = React.useState(true);
+  const url = (location) => (`https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_key}`)
+
+
 
   React.useEffect(() => {
-    axios
-      .get(
-        `${proxy}pro.openweathermap.org/data/2.5/forecast/hourly?q=Dnipro&appid=b8988efec93cd285602a1f2dda449964`,
-      )
-      .then((responce) => console.log(responce));
+    const getWetherByLocation = async (location) => {
+      setLoading(true);
+      const resp = await fetch(url(location));
+      const respData = await resp.json();
+
+      console.log(respData);
+    }
+
+      getWetherByLocation('dnipro');
+      setLoading(false);
   }, []);
 
+  console.log(loading);
   return (
     <div className="App">
       kuku
